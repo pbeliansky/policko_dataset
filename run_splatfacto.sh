9f20d282c2677ce8021c1b5a1f6a9e0a2935fca9
Patrik Beliansky <patrik.beliansky10@gmail.com>
2025-07-28 00:28:21 +0200

    sh

---
+++ run_splatfacto.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+exp="${1:-testik}"
+
+prune_start="${2:-50000}"
+prune_each="${3:-5000}"
+prune_qant="${4:-0.4}"
+
+data="${5:-external://blender/lego}"
+
+proj_name="${6:-nerfbaselines-test}"
+
+output_name="${proj_name}/${exp}"
+
+#!/bin/bash
+sbatch <<EOT
+#!/bin/bash
+
+#SBATCH --job-name=tetra_benchmark
+#SBATCH --account=OPEN-29-7
+#SBATCH --partition qgpu
+#SBATCH --time 2-00:00:00
+
+ml --force purge
+
+apptainer exec --userns --writable --nv --no-home --cleanenv --home /home/user tetra.sbox \
+  ns-train splatfacto --vis wandb \
+    \
+    --data pole_data/policko_dataset/
+
+EOT
